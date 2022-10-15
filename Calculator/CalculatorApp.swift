bdf750af037d39b5206fac7292d8cb4168938131
YuGeonHui <ghyu_0906@naver.com>
2022-10-15 13:38:53 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by geonhui Yu on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
