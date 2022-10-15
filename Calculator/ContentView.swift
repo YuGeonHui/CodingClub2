db707e534deec7d54929a2e9a81f5c6fd2d0d71a
YuGeonHui <ghyu_0906@naver.com>
2022-10-16 00:46:02 +0900

    계산기 View 개발 완료

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,15 +7,103 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum ButtonType: Hashable {
+    
+    case number(String?)
+    case calculation(String?)
+    case unit(String?)
+    
+    var buttonDisplayName: String? {
+        
+        switch self {
+        case .number(let number):
+            return number
+            
+        case .calculation(let calculate):
+            return calculate
+            
+        case .unit(let unit):
+            return unit
         }
-        .padding()
+    }
+    
+    var backgroundColor: Color {
+        
+        switch self {
+        case .number:
+            return .gray
+        case .calculation:
+            return .orange
+        case .unit:
+            return .yellow
+        }
+    }
+    
+    var forgroundColor: Color {
+        
+        switch self {
+        case .number:
+            return .white
+        case .calculation, .unit:
+            return .black
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var totalNumber: String = "0"
+    
+    private let buttonData: [[ButtonType]] = [
+        [.unit("C"), .unit("/"), .unit("%"), .calculation("=")],
+        [.number("7"), .number("8"), .number("9"), .calculation("X")],
+        [.number("4"), .number("5"), .number("6"), .calculation("-")],
+        [.number("1"), .number("2"), .number("3"), .calculation("+")],
+        [.number("0"), .number("0"), .number(","), .calculation("=")],
+    ]
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Color.black.ignoresSafeArea()
+            
+            
+            VStack {
+            
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Text(totalNumber)
+                        .padding()
+                        .font(.system(size: 73))
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(buttonData, id: \.self) { line in
+                    
+                    HStack {
+                        
+                        ForEach(line, id: \.self) { row in
+                            
+                            Button {
+                                
+                                totalNumber += row.buttonDisplayName ?? ""
+                                
+                            } label: {
+                                Text(row.buttonDisplayName ?? "")
+                                    .frame(width: 80, height: 80)
+                                    .background(row.backgroundColor)
+                                    .cornerRadius(40)
+                                    .foregroundColor(row.forgroundColor)
+                                    .font(.system(size: 33))
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
